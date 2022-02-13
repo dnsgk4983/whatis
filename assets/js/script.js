0195a0e3025336707dc737ddb8abc0a0365cc5e6
booldook <64947285+dnsgk4983@users.noreply.github.com>
2022-02-14 02:44:39 +0900

    0214

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,48 @@
 const visual = document.querySelector('.wadiz__visual');
+const visualImage = document.querySelector('.wadiz__visual__image');
 const visualLabel = document.querySelector('.wadiz__visual__label');
 const visualTitle = document.querySelector('.wadiz__visual__title');
+const visualText01 = document.querySelector('.wadiz__visual__text.text01');
+const visualText02 = document.querySelector('.wadiz__visual__text.text02');
 let zoom = 1;
-let base = 1;
-const speed = 0.01;
+let base = 0;
+const ZOOM_SPEED = 0.00033;
 
-console.log(visual.clientHeight);
 document.addEventListener(`wheel`, (e) => {
-    let value = window.scrollY;
-
-    console.log(e.deltaY);
-    if (value > (visual.clientHeight) / 3) {
-        visualLabel.style.opacity = `${base -= (speed * 3)}`;
-    } else if (value > ((visual.clientHeight) / 3) * 2) {
-        visualLabel.style.opacity = `0.5`;
+    let scroll = window.scrollY;
+    if (scroll < (visual.offsetHeight / 3)) {
+        visualImage.style.transform = `scale(${1 + (scroll * ZOOM_SPEED)})`;
+        visualImage.classList.remove('black');
+        visualLabel.style.opacity = 1 - (scroll * 0.001);
+        visualTitle.style.opacity = 1 - (scroll * 0.001);
+        visualText01.classList.remove('show');
+    } else if (scroll > (visual.offsetHeight / 3) && scroll < ((visual.offsetHeight) / 3) * 2) {
+        visualImage.classList.add('black');
+        visualLabel.style.opacity = 0.5;
+        visualTitle.style.opacity = 0;
+        visualText01.classList.add('show');
+        visualText02.classList.remove('show');
+    } else if (scroll > ((visual.offsetHeight) / 3) * 2) {
+        visualText01.classList.remove('show');
+        visualText02.classList.add('show');
+        visual.classList.remove('end');
     } else {
-        visualLabel.style.opacity = `${base += (speed * 3)}`;
+        visual.classList.add('end')
+    }
+
+    console.log(((visual.offsetHeight) / 3) * 1.2);
+});
+
+const reivewSwiper = new Swiper('.wadiz__review .swiper', {
+    slidesPerView: 3,
+    spaceBetween: 16,
+    breakpoint: {
+        768: {
+            slidesPerView: 1,
+        },
+        769: {
+            slidesPerView: 3,
+        },
     }
 })
+
